bac37eb88aba19aa2a8c81a5a83ee4aaeb950b1e
yongheng <az456005@163.com>
2024-05-29 13:28:05 +0800

    add demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-import 'game.dart';
+import 'game/custom_game.dart';
 
 void main() {
   runApp(GameWidget(game: CustomGame()));
