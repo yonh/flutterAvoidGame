63103dcda53c41de54db111d53473d00745d1b1d
yongheng <az456005@163.com>
2024-05-29 13:22:23 +0800

    init Game

---
+++ lib/game.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class CustomGame extends FlameGame {
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+}
